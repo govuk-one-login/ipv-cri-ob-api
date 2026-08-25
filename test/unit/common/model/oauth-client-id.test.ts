e3830670912c4d7de352e4b2462e5f0ed0d60f98
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2026-08-25 13:23:54 +0100

    [squash] extended test coverage

--- test/unit/common/model/oauth-client-id.test.ts
+++ test/unit/common/model/oauth-client-id.test.ts
@@ -1,12 +1,19 @@
-import { getTokenProfileForClientId } from '@common/model/oauth-client-id'
+import { getTokenProfileForClientId, OAuthClientId } from '@common/model/oauth-client-id'
 import { TokenProfile } from '@lib/token-rotator/model/token-profile'
 import { describe, expect, it } from 'vitest'
 
 describe('getTokenProfileForClientId', () => {
-  it('returns correct token profile when client is known', () => {
-    expect(getTokenProfileForClientId('ipv-core-stub')).toBe(TokenProfile.STUB)
-    expect(getTokenProfileForClientId('ipv-core')).toBe(TokenProfile.LIVE)
-    expect(getTokenProfileForClientId('ipv-core-stub-aws-build_3rdparty')).toBe(TokenProfile.UAT)
+  it.each([
+    [OAuthClientId.IPV_CORE, TokenProfile.LIVE],
+    [OAuthClientId.IPV_CORE_STUB, TokenProfile.STUB],
+    [OAuthClientId.IPV_CORE_STUB_AWS_BUILD, TokenProfile.STUB],
+    [OAuthClientId.IPV_CORE_STUB_AWS_BUILD_THIRD_PARTY, TokenProfile.UAT],
+    [OAuthClientId.IPV_CORE_STUB_AWS_PROD, TokenProfile.STUB],
+    [OAuthClientId.IPV_CORE_STUB_AWS_PROD_THIRD_PARTY, TokenProfile.UAT],
+    [OAuthClientId.IPV_CORE_STUB_PRE_PROD_AWS_BUILD, TokenProfile.LIVE],
+    [OAuthClientId.IPV_CORE_THIRD_PARTY_STUBS, TokenProfile.STUB]
+  ])('maps client id %s to profile %s', (clientId, expectedProfile) => {
+    expect(getTokenProfileForClientId(clientId)).toBe(expectedProfile)
   })
   it('throws when client is unknown or missing', () => {
     expect(() => getTokenProfileForClientId('crumbs')).toThrow('Unknown OAuth Client ID')
